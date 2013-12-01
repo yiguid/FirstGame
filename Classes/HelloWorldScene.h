2d9beb542cad9c754b64b170b19c4de9f2062c0f
yiguid <yiguid@gmail.com>
2013-12-01 22:49:20 +0800

    initial commit

---
+++ Classes/HelloWorldScene.h
@@ -0,0 +1,32 @@
+#ifndef __HELLOWORLD_SCENE_H__
+#define __HELLOWORLD_SCENE_H__
+
+#include "cocos2d.h"
+
+using namespace cocos2d;
+
+class HelloWorld : public cocos2d::CCLayer
+{
+public:
+    // Method 'init' in cocos2d-x returns bool, instead of 'id' in cocos2d-iphone (an object pointer)
+    virtual bool init();
+    virtual void registerWithTouchDispatcher();
+    virtual bool ccTouchBegan(CCTouch* touch, CCEvent* event);
+    virtual void ccTouchMoved(CCTouch* touch, CCEvent* event);
+    virtual void ccTouchEnded(CCTouch* touch, CCEvent* event);
+    virtual void ccTouchCancelled(CCTouch* touch, CCEvent* event);
+    // there's no 'id' in cpp, so we recommend to return the class instance pointer
+    static cocos2d::CCScene* scene();
+    CCSprite *hero;
+    bool isControl;
+    float deltax;
+    float deltay;
+    
+    // a selector callback
+    void menuCloseCallback(CCObject* pSender);
+
+    // preprocessor macro for "static create()" constructor ( node() deprecated )
+    CREATE_FUNC(HelloWorld);
+};
+
+#endif // __HELLOWORLD_SCENE_H__
